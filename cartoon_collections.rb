54fcceaa1b06eec8f367b8d1a6f1cda736deacc9
Ryan Locascio <loc.fitness1@gmail.com>
2020-06-29 17:52:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,8 @@
 def greet_characters(array)
-  array.each do |array|
-    puts "Hello #{array}"
+  
 end
 
 
 def list_dwarves(array)
-  array.each_with_index do |array|
-    puts "#{array}"
+
 end
